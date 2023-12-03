b77d3779266deeed7fe74ad1cbbb2e73182d94a5
Doomshap <152163342+Doomshap@users.noreply.github.com>
2023-12-03 15:18:58 +1200

    added step 6
    --age groups
    Number of buyers in different age groups.
    
    --customers by month
    Data on the number of unique buyers and the revenue they brought in.
    
    --special offer
    Data about customers whose first purchase was during promotions.

--- queries.sql
+++ queries.sql
@@ -8,6 +8,7 @@ inner join employees on sales.sales_person_id = employees.employee_id
 group by concat(first_name,' ', last_name)
 order by (3) desc limit 10
 
+
 --lowest average income
 with tab as(
 select distinct concat(first_name, ' ', last_name) as name,
@@ -19,6 +20,7 @@ inner join employees on sales.sales_person_id = employees.employee_id)
 select name, round(personal_avg) as average_income
 from tab where (select avg(personal_avg) from tab)>personal_avg
 order by (2)
+
 
 --day of the week income
 with tab as(
@@ -36,3 +38,45 @@ select
 name, week_day_char as weekday, income
 from tab
 order by week_day_num
+
+
+--age groups
+select distinct
+case when age between 16 and 25 then '16-25'
+     when age between 26 and 45 then '26-40'
+     when age>40 then '40+' end as age_category,
+     count(customer_id) as count
+from customers
+group by (age_category)
+
+
+--customers by month
+SELECT to_char(sale_date, 'YYYY-MM') as date,
+count(customer_id) as total_customers,
+sum(quantity*price) as income
+FROM sales
+inner join products on sales.product_id = products.product_id
+group by to_char(sale_date, 'YYYY-MM')
+order by date
+
+
+--special offer
+with tab as(
+select
+sales.customer_id as id,
+concat(customers.first_name, ' ', customers.last_name) as customer,
+products.price as price,
+concat(employees.first_name, ' ', employees.last_name) as seller,
+sale_date,
+ROW_NUMBER() over (partition by concat(customers.first_name, ' ', customers.last_name) order by (1), sale_date)
+from sales
+inner join products on sales.product_id = products.product_id
+inner join customers on sales.customer_id = customers.customer_id 
+inner join employees on sales.sales_person_id = employees.employee_id
+order by (1), sale_date
+)
+
+select customer, sale_date, seller
+from tab
+where row_number=1 and price=0
+order by id
